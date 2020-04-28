25cb33054c1597f50f11db85e731fb71b10e700a
Lalib <louis.alib19@gmail.com>
2020-04-28 11:53:04 +0200

    routes programs and exo

--- models/Program.js
+++ models/Program.js
@@ -4,9 +4,15 @@ const Schema = mongoose.Schema;
 const programSchema = new Schema({
 	name: String,
 	description: String,
-	exercisesList: [ { type: Schema.Types.ObjectId, ref: 'exercise' } ],
-	creater: String,
-	likes: [ { type: Schema.Types.ObjectId, ref: 'like' } ]
+	exercisesList: [{
+		type: Schema.Types.ObjectId,
+		ref: 'exercise'
+	}],
+	creator: String,
+	likes: [{
+		type: Schema.Types.ObjectId,
+		ref: 'like'
+	}]
 });
 
 const program = mongoose.model('program', programSchema);
--- routes/programs.js
+++ routes/programs.js
@@ -1,9 +1,18 @@
 const express = require("express");
 const router = new express.Router();
+const program = require("../models/Program")
+
 
 router.get("/programs", (req, res, next) => {
-    res.render("programs");
+    program.find()
+        .then(dbResult => {
+            res.render("programs", {
+                programs: dbResult
+            });
+        })
+        .catch(dbErr => {
+            console.log(dbErr);
+        });
 });
-
 
 module.exports = router;
